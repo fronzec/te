fb2aba2ebece9f11d455b7367621b5b5f5f00e9b
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2018-05-28 15:39:30 -0500

    Initial commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float rotationAngle = 150f;
+
+    [SerializeField] private float deltaTraslation = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotationAngle;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * deltaTraslation;
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
